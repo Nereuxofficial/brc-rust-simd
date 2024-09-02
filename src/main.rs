f2fb0bf365a771f6e0dd260d6690bd6cd2bd4116
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2024-09-02 18:04:01 +0200

    feat: portable_simd version of average_iterator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+#![feature(portable_simd)]
+#![feature(iter_array_chunks)]
 use std::collections::HashMap;
 use std::io::BufRead;
 use std::ops::Div;
+use std::simd::num::{SimdFloat, SimdInt};
+use std::simd::{f32x8, i32x8};
 use std::{error::Error, io::BufReader};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -33,7 +37,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             format!(
                 "{name};{};{};{}\n",
                 fmt_num(*val.iter().min().unwrap()),
-                fmt_num(get_average(val.iter())),
+                fmt_num(get_average_simd(&mut val.iter())),
                 fmt_num(*val.iter().max().unwrap())
             )
         })
@@ -49,7 +53,6 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn fmt_num(num: i32) -> String {
     let num_str = num.to_string();
     let num_chars = num_str.chars().rev().enumerate();
-    println!("{:?}", num_chars);
     let mut s = String::with_capacity(4);
     for (i, d) in num_chars {
         if i == 2 {
@@ -60,6 +63,29 @@ fn fmt_num(num: i32) -> String {
     s.chars().rev().collect()
 }
 
+fn get_average_simd<'a, I: Iterator<Item = &'a i32>>(iter: &mut I) -> i32 {
+    let mut count = 0;
+    let sum_matrix = iter
+        .copied()
+        .array_chunks::<8>()
+        .map(i32x8::from_array)
+        .fold(i32x8::splat(0), |acc, x| {
+            count += 4;
+            acc + x
+        });
+    let mut remain = 0;
+    for _ in 0..8 {
+        if let Some(v) = iter.next() {
+            remain += v;
+        } else {
+            break;
+        }
+    }
+    (sum_matrix.reduce_sum() + remain)
+        .checked_div(count)
+        .unwrap_or(0)
+}
+
 fn get_average<'a, I: Iterator<Item = &'a i32>>(iter: I) -> i32 {
     let mut count = 0;
     iter.fold(0, |acc, x| {
@@ -68,13 +94,12 @@ fn get_average<'a, I: Iterator<Item = &'a i32>>(iter: I) -> i32 {
     })
     .div(count)
 }
-
 #[cfg(debug_assertions)]
-mod tests{
+mod tests {
     use super::*;
-    
+
     #[test]
-    fn test_fmt_num(){
+    fn test_fmt_num() {
         let num = -789;
         let num2 = 678;
         println!("num: {}, num2: {}", fmt_num(num), fmt_num(num2));
